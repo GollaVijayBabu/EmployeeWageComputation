7a9fa9829facc7de87fc6e9d69932a7da68e2673
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-10-27 00:41:00 -0700

    Added empployee wage computation

--- Program.cs
+++ Program.cs
@@ -4,9 +4,31 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee wage computation");
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            UC_1 uc1 = new UC_1();
+            uc1.Attendance();
+
+            UC_2 uc2 = new UC_2();
+            uc2.DailyWage();
+
+            UC_3 uc3 = new UC_3();
+            uc3.PartTime();
+
+            UC_4 uc4 = new UC_4();
+            uc4.Case();
+
+            UC_5 uc5 = new UC_5();
+            uc5.MonthlyWage();
+
+            UC_6 uc6 = new UC_6();
+            uc6.Condition();
+
+            UC_7 uc7 = new UC_7();
+            uc7.ComputeEmpWage();
         }
     }
 }
---
+++ UC_4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class UC_4
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
+        public void Case()
+        {
+            int empHrs = 0, empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee Full TIME Present");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee PART TIME Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Daily Wage :" + empWage);
+        }
+    }
+}
